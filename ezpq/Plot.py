a2dbead68800bf5f00c0d1f1c96ba5b852319c11
Donald Mellenbruch <hello@Fresh2.dev>
2019-01-21 00:00:00 -0600

    generalized plot faceting; moved `ezpq.Plot()` parameters to `ezpq.Plot.build()`.

--- ezpq/Plot.py
+++ ezpq/Plot.py
@@ -1,38 +1,20 @@
 class Plot():
     """Functions to produce Gantt chart from completed job schedules."""
 
-    def __init__(self, jobs, color_by='qid', show_legend=True, bar_width=1, title=None):
+    def __init__(self, jobs):
         """Some description.
 
         Args:
             jobs:
                 - Accepts: list of dictionary objects, as returned by ezpq.Queue.collect()
-            color_by: controls the column to use for coloring the bars.
-                - Accepts: one of 'priority', 'cancelled', 'exitcode', 'name', 'output'
-                - Default: 'priority'
-            show_legend: controls whether the legend is drawn.
-                - Accepts: bool
-                - Defualt: True
-            bar_width: controls the bar width
-                - Accepts: float
-                - Default: 1
-            title: optional title to be drawn above the plot.
-                - Accepts: str, None
-                - Default: None
-
         Returns:
             self
         """
 
-        assert(color_by in ['qid', 'priority', 'cancelled', 'exitcode', 'name', 'output'])
-        self.color_by = color_by
-        self.show_legend = show_legend
-        self.bar_width = bar_width
-        self.title = title
-
         import pandas
 
         df = pandas.DataFrame(jobs)
+        df.lane.fillna(value='', inplace=True)
         min_time = df['submitted'].min()
         df['submitted_offset'] = df['submitted'] - min_time
         df['started_offset'] = df['started'] - min_time
@@ -40,7 +22,8 @@ class Plot():
         df['processed_offset'] = df['processed'] - min_time
         self.jobs_df = df
 
-    def _plot_theme(self, grid_axis='both', grid_lines='both'):
+    @staticmethod
+    def _plot_theme(grid_axis='both', grid_lines='both'):
         """Internal function provides consistent theme across plots.
         Currently a slightly modified version of theme_bw() with configurable grid lines.
 
@@ -87,33 +70,56 @@ class Plot():
                           axis_line = gg.element_line(color = "black"),
                           **grid_opt)
 
-    def build(self):
+    def build(self, color_by='qid', facet_by='qid', facet_scale='fixed', show_legend=True, bar_width=1, title=None):
         """Produces a plot based on the data and options provided to a `ezpq.Plot()` object.
 
+        Args:
+            color_by: controls the column to use for coloring the bars.
+                - Accepts: one of 'qid', 'priority', 'lane', 'cancelled', 'exitcode', 'name', 'output'
+                - Default: 'qid'
+            facet_by: controls the column to use for facetting the plot.
+                - Accepts: one of 'qid', 'priority', 'lane', 'cancelled', 'exitcode', 'name', 'output'
+                - Default: 'qid'
+            facet_scale: controls the scale of the x/y axis across facets.
+                - Accepts: one of 'fixed', 'free', 'free_x', 'free_y'
+                - Default: 'fixed'
+            show_legend: controls whether the legend is drawn.
+                - Accepts: bool
+                - Default: True
+            bar_width: controls the bar width
+                - Accepts: float
+                - Default: 1
+            title: optional title to be drawn above the plot.
+                - Accepts: str, None
+                - Default: None
         Returns:
             The plot produced from plotnine.ggplot().
         """
 
+        assert(color_by in ['qid', 'priority', 'lane', 'cancelled', 'exitcode', 'name', 'output'])
+        assert(facet_by in ['qid', 'priority', 'lane', 'cancelled', 'exitcode', 'name', 'output'])
+        assert(facet_scale in ['fixed', 'free', 'free_x', 'free_y'])
+
         import plotnine as gg
 
-        df2 = self.jobs_df.loc[:, set(['qid', 'id', self.color_by, 'submitted_offset', 'started_offset', 'ended_offset', 'processed_offset'])].melt(id_vars=set(['qid', 'id', self.color_by]))
+        df2 = self.jobs_df.loc[:, set(['qid', 'id', color_by, facet_by, 'submitted_offset', 'started_offset', 'ended_offset', 'processed_offset'])].melt(id_vars=set(['qid', 'id', color_by, facet_by]))
 
         df_submit_start = df2[(df2['variable'] == 'submitted_offset') | (df2['variable'] == 'started_offset')]
         df_start_end = df2[(df2['variable'] == 'started_offset') | (df2['variable'] == 'ended_offset')]
         df_end_processed = df2[(df2['variable'] == 'ended_offset') | (df2['variable'] == 'processed_offset')]
 
         labs = { 'x': 'duration', 'y': 'job id' }
-        if self.title is not None:
-            labs['title'] = self.title
+        if title is not None:
+            labs['title'] = title
 
         return gg.ggplot(gg.aes(x='value', y='id', group='factor(id)')) + \
-                gg.geom_line(df_submit_start, color='gray', size=self.bar_width, alpha=0.25) + \
+                gg.geom_line(df_submit_start, color='gray', size=bar_width, alpha=0.25) + \
                 gg.geom_line(df_start_end,
-                             gg.aes(color='factor({})'.format(self.color_by)),
-                             size=self.bar_width, show_legend=bool(self.show_legend)) + \
-                gg.geom_line(df_end_processed, color='gray', size=self.bar_width, alpha=0.25) + \
+                             gg.aes(color='factor({})'.format(color_by)),
+                             size=bar_width, show_legend=bool(show_legend)) + \
+                gg.geom_line(df_end_processed, color='gray', size=bar_width, alpha=0.25) + \
                 gg.labs(**labs) + \
-                gg.labs(color=self.color_by) + \
-                self._plot_theme(grid_axis='x') + \
+                gg.labs(color=color_by) + \
+                Plot._plot_theme(grid_axis='x') + \
                 gg.scale_color_hue(h=.65) + \
-                gg.facet_grid(facets='qid~')
+                gg.facet_grid(facets='{}~'.format(facet_by), labeller='label_both', scales=facet_scale, as_table=True)
