d759d7b9dd17d0df2be045607d2baef20e8f2faf
Donald Mellenbruch <hello@Fresh2.dev>
2019-01-25 00:00:00 -0600

    added/revised tests.

--- test/test_ezpq.py
+++ test/test_ezpq.py
@@ -5,21 +5,21 @@ from threading import Thread
 
 from _context import ezpq
 
-def return_i(i=1):
-    return i
+def return_me(x=1):
+    return x
 
 class TestEZPQ(unittest.TestCase):
 
     def setUp(self):
-        self.Q = ezpq.Queue(job_runner=Process, auto_start=True)
+        self.Q = ezpq.Queue(job_runner=Process, auto_start=True, n_workers=5)
         self.input = tuple(range(100))
 
     def test_priority(self):
         self.Q._stop()
         
-        for i in self.input:
-            self.Q.put(function=return_i, args=i,
-                        priority=-i) # should result in reversed inputs.
+        for x in self.input:
+            self.Q.put(function=return_me, args=x,
+                        priority=-x) # should result in reversed inputs.
         
         self.Q.start()
         self.Q.wait()
@@ -29,9 +29,19 @@ class TestEZPQ(unittest.TestCase):
         self.assertEqual(tuple(reversed(self.input)), out_list)
 
     def test_map(self):               
-        job_data = self.Q.map(function=return_i, iterable=self.input, ordered=True)
+        job_data = self.Q.map(function=return_me, iterable=self.input)
 
         out_list = tuple(job['output'] for job in job_data)
+
+        self.assertEqual(self.input, out_list)
+
+    def test_lanes(self):
+        for i, x in enumerate(self.input):
+            self.Q.put(function=return_me, args=x,
+                        lane = i % self.Q.n_workers()) # returns in order
+
+        self.Q.wait()
+        out_list = tuple(x['output'] for x in self.Q.collect())
 
         self.assertEqual(self.input, out_list)
 
